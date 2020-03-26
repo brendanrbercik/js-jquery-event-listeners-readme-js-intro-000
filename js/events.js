77414bab8fc529a3d7a5ea0e0148e8f351d0f8e4
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-26 18:02:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $(document).on("keydown", function() {
+  $(document).on("keydown", function(key) {
     if (key.which == 071) {
       alert('G key pressed!');
     }
